47a6491108616d68e0e68dc7f8b200fe3519ad17
Caedin Cook <caco5646@colorado.edu>
2023-10-13 13:44:22 +0200

    added timer to last page

--- script/menu.js
+++ script/menu.js
@@ -1,4 +1,107 @@
+var activeMenu = "menuSelection";
+let menus = ["menuSelection", "menuReview", "menuConfirm"];
+var timerInterval;
+var timerDuration = "10:00";
 $(document).ready(function () {
     //get all the menu item selection divs
     $("#selectionBall").addClass("active");
+    var setBall = function (ball, width) {
+        clearInterval(timerInterval);
+        $(".progress-ball").removeClass("active");
+        $(ball).addClass("active");
+        $("#progress").css("width", width);
+    };
+    var setMenu = function (menu) {
+        $("#" + activeMenu).removeClass("active");
+        $("#" + menu).addClass("active");
+        activeMenu = menu;
+    };
+    $("#selectionBall").click(function () {
+        setBall(this, "30%");
+
+        //set menuSelection to active
+        setMenu("menuSelection");
+
+    });
+    $("#reviewBall").click(function () {
+        setBall(this, "60%");
+        $("#selectionBall").addClass("active");
+
+        //set menuReview to active
+        setMenu("menuReview");
+
+    });
+    $("#confirmBall").click(function () {
+        $(".progress-ball").removeClass("active");
+        $("#selectionBall").addClass("active");
+        $("#reviewBall").addClass("active");
+        $("#progress").css("width", "80%");
+        
+        //set menuConfirm to active
+        setMenu("menuConfirm");
+        $("#timer").text(timerDuration);
+
+        handleTimer();
+
+
+    });
 });
+
+//function for when the selectionBall is clicked 
+
+//functino to decrease timer
+function handleTimer() {
+    var timer = $("#timer");
+    var time = timer.text();
+    var timeSplit = time.split(":");
+    var minutes = parseInt(timeSplit[0]);
+    var seconds = parseInt(timeSplit[1]);
+    seconds += minutes * 60;
+    //now that we have the seconds we can calculate the width to move the progress bar
+    var percentagePerSecond = 20 / seconds;
+    console.log(percentagePerSecond);
+    clearInterval(handleTimer);
+    timerInterval = setInterval(function () {
+        //get the timer
+        var timer = $("#timer");
+        //get the time
+        var time = timer.text();
+        //split the time
+        var timeSplit = time.split(":");
+        //get the minutes and seconds
+        var minutes = parseInt(timeSplit[0]);
+        var seconds = parseInt(timeSplit[1]);
+        //decrease the seconds
+        seconds--;
+        //if seconds is less than 0
+        if (seconds < 0) {
+            //decrease the minutes
+            minutes--;
+            //set the seconds to 59
+            seconds = 59;
+        }
+        //if minutes is less than 0
+        if (minutes < 0) {
+            //set the minutes to 0
+            minutes = 0;
+            //set the seconds to 0
+            seconds = 0;
+            //stop the timer
+            clearInterval(timerInterval);
+            $("#confirmBall").addClass("active");
+        }
+        //if seconds is less than 10
+        if (seconds < 10) {
+            //add a 0 to the front of the seconds
+            seconds = "0" + seconds;
+        }
+        //set the timer text
+        timer.text(minutes + ":" + seconds);
+        //set the progess bar
+        var progressWidth = +(100*$("#progress").width()/$("#progress").parent().width()).toFixed(2);
+        // console.log(progressWidth);
+        var newWidth = progressWidth + percentagePerSecond;
+        console.log(newWidth);
+        $("#progress").css("width", (newWidth) + "%");
+    }, 1000);
+}
